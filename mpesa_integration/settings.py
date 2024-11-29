ac7f97daac6cc3fa215c4e41d6de000daea6db1c
ianmukuna97 <ianmukuna97@gmail.com>
2024-11-29 08:58:58 +0300

    Update settings.py

--- mpesa_integration/settings.py
+++ mpesa_integration/settings.py
@@ -132,18 +132,18 @@ MPESA_ENVIRONMENT = 'sandbox'
 
 # Credentials for the daraja app
 
-# MPESA_CONSUMER_KEY = 'YtiYp0DBCscmosYgJqNIhAiKKiAFSVhVTOQymSE1ZBboHNoa'
-# MPESA_CONSUMER_SECRET = 'UDV5qVwxh4vny0cCUiEgAZ3lj2ZCe5zn3FfRSSgcvQZFyiGtDNvyZKQNhNStEYWI'
+# MPESA_CONSUMER_KEY = ''
+# MPESA_CONSUMER_SECRET = ''
 
 #Shortcode to use for transactions. For sandbox  use the Shortcode 1 provided on test credentials page
 
-MPESA_SHORTCODE = '174379'
+MPESA_SHORTCODE = ''
 
 # Shortcode to use for Lipa na MPESA Online (MPESA Express) transactions
 # This is only used on sandbox, do not set this variable in production
 # For sandbox use the Lipa na MPESA Online Shorcode provided on test credentials page
 
-MPESA_EXPRESS_SHORTCODE = '174379'
+MPESA_EXPRESS_SHORTCODE = ''
 
 # Type of shortcode
 # Possible values:
@@ -156,7 +156,7 @@ MPESA_SHORTCODE_TYPE = 'paybill'
 # Sandbox passkey is available on test credentials page
 # Production passkey is sent via email once you go live
 
-# MPESA_PASSKEY = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+# MPESA_PASSKEY = ''
 
 # Username for initiator (to be used in B2C, B2B, AccountBalance and TransactionStatusQuery Transactions)
 
